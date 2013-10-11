25a31184eed429bd9671eddd7998d0321eba1cea
Thiago de Arruda <tpadilha84@gmail.com>
2013-10-11 10:12:22 -0300

    Implemented {Filter,Find}Stream and added a
    'close' method to PullStream to enable closing the data source prematurely

---
+++ test/filter.js
@@ -0,0 +1,26 @@
+var spipe = require('../lib/s-pipe');
+var range = require('../lib/range');
+var filter = require('../lib/filter');
+
+
+function even(n) { return n % 2 === 0; }
+
+
+runMocha({
+  'FilterStream': {
+    'filter values synchronously': function() {
+      deepEqual(spipe(range(1, 10))(filter, even)(), [2, 4, 6, 8, 10]);
+    },
+
+    'lazily evaluate the stream': function(done) {
+      var result = [];
+      var stream = spipe(range(1, 10), true)(filter, even)();
+
+      stream.on('data', function(n) { result.push(n); });
+      stream.on('end', function() {
+        deepEqual(result, [2, 4, 6, 8, 10]);
+        done();
+      });
+    }
+  }
+});
---
+++ test/find.js
@@ -0,0 +1,26 @@
+var spipe = require('../lib/s-pipe');
+var range = require('../lib/range');
+var find = require('../lib/find');
+
+
+function predicate(n) { return n % 30 === 0; }
+
+
+runMocha({
+  'FindStream': {
+    'find values synchronously': function() {
+      deepEqual(spipe(range(1, 10000000000))(find, predicate)(), 30);
+    },
+
+    'lazily evaluate the stream': function(done) {
+      var result = [];
+      var stream = spipe(range(1, 10000000000), true)(find, predicate)();
+
+      stream.on('data', function(n) { result.push(n); });
+      stream.on('end', function() {
+        deepEqual(result, [30]);
+        done();
+      });
+    }
+  }
+});
