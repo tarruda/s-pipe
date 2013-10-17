8d682aaa4fd7fb430a7951cff447f228a2e4bced
Thiago de Arruda <tpadilha84@gmail.com>
2013-10-17 12:16:28 -0300

    Add special EOF token handling

--- test/parse.js
+++ test/parse.js
@@ -5,17 +5,34 @@ var ParseStream = require('../lib/parse');
 
 var TestParseStream = ParseStream.extend({
   grammar: {
-    start: 'E',
+    start: 'program',
 
     rules: {
-      E: [
-        'E operator B',
-        'B'
+      program: [
+        'expression EOF'
       ],
 
-      B: [
-        '0',
-        '1'
+      expression: [
+        'literal',
+        'expression operator literal'
+      ],
+
+      literal: [
+        'digit',
+        'letter'
+      ],
+
+      digit: [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+      ],
+
+      letter: [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+      ],
+
+      bool: [
+        'true', 'false'
       ],
 
       operator: [
@@ -31,27 +48,42 @@ function parse() {
 }
 
 
+function p(str) {
+  return spipe(str)
+              (chars)
+              (reject, function(c) { return (/\s/).test(c); })
+              (parse)();
+}
+
 runMocha({
   'TokenStream': {
 
     'parse synchronously': function() {
-      deepEqual(spipe('0+1+0-1+1')
-               (chars)
-               (reject, function(c) { return (/\s/).test(c); })
-               (parse)(),
-               [[[[['0', '+', '1'], '+', '0'], '-', '1'], '+', '1']]);
+      deepEqual(p('0+1+0-1+1'),
+               [[[[[['0', '+', '1'], '+', '0'], '-', '1'], '+', '1'], 'EOF']]);
     },
 
-    'syntax errors': function() {
+    'syntax error 1': function() {
       var throwed = false;
 
       try {
-        spipe('0+1*0-1+1')
-              (chars)
-              (reject, function(c) { return (/\s/).test(c); })
-              (parse)();
+        p('0+1*0-1+1');
       } catch (e) {
-        equal(e.message, "Unexpected symbol '*'. Expecting 'operator'");
+        equal(e.message, "Unexpected '*'. Expecting 'operator' or 'EOF'");
+        throwed = true;
+      }
+
+      if (!throwed)
+        throw new Error('Expecting exception');
+    },
+
+    'syntax error 2': function() {
+      var throwed = false;
+
+      try {
+        p('0+1+--1+1');
+      } catch (e) {
+        equal(e.message, "Unexpected '-'. Expecting 'literal'");
         throwed = true;
       }
 
